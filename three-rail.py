23c6b4fdb46decef350df542854a75f7a79832e1
sena <senaredline@gmail.com>
2018-05-11 22:11:18 +0900

    make it better

--- three-rail.py
+++ three-rail.py
@@ -1,13 +1,15 @@
 import sys
 
-b_str=sys.argv[1]
+rail=sys.argv[1]
+b_str=sys.argv[2]
 a_str=''
-if len(b_str)%3!=0:
-    for num in range(len(b_str)//3):
-        a_str+=b_str[num:(len(b_str)%3)*(len(b_str)//3+1):len(b_str)//3+1]
-        a_str+=b_str[(len(b_str)%3)*(len(b_str)//3+1)+num::len(b_str)//3]
-    a_str+=b_str[len(b_str)//3:(len(b_str)%3)*(len(b_str)//3+1):len(b_str)//3+1]
-if len(b_str)%3==0:
-    for num in range(len(b_str)//3):
-        a_str+=b_str[num::int(len(b_str)/3)]
+
+if len(b_str)%int(rail)!=0:
+    for num in range(len(b_str)//int(rail)):
+        a_str+=b_str[num:(len(b_str)%int(rail))*(len(b_str)//int(rail)+1):len(b_str)//int(rail)+1]
+        a_str+=b_str[(len(b_str)%int(rail))*(len(b_str)//int(rail)+1)+num::len(b_str)//int(rail)]
+    a_str+=b_str[len(b_str)//int(rail):(len(b_str)%int(rail))*(len(b_str)//int(rail)+1):len(b_str)//int(rail)+1]
+if len(b_str)%int(rail)==0:
+    for num in range(len(b_str)//int(rail)):
+        a_str+=b_str[num::int(len(b_str)/int(rail))]
 print(a_str)
